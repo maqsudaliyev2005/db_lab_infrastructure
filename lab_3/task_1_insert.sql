44d37ce1e1544ca51462e13481c3076027865df1
maqsudaliyev2005 <maqsud.aliyev.2005@gmail.com>
2026-05-06 11:18:14 +0400

    Add sample data for users and posts

--- lab_3/task_1_insert.sql
+++ lab_3/task_1_insert.sql
@@ -1,1 +1,2 @@
-
+INSERT INTO `users` (`id`, `username`, `role`) VALUES (10, 'Test_User', 'member');
+INSERT INTO `posts` (`id`, `title`, `user_id`, `status`) VALUES (11, 'Advanced SQL Post', 10, 'published');
